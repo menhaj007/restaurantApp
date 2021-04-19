e9164e1f8b5e4f78a27372a22d4d848804677a28
menhaj <menhaj007@yahoo.com>
2021-04-19 15:56:35 -0400

    working on form

--- src/index.js
+++ src/index.js
@@ -34,20 +34,27 @@ function displayFood() {
 }
 
 function addToSubContainer(food) {
-    const foodName = document.createElement('h4')
+    const foodName = document.createElement('h3')
     const foodImage = document.createElement('img')
     const foodCal = document.createElement('h4')
     const foodRev = document.createElement('h4')
+    const foodCom = document.createElement('h4')
         
     foodImage.src = food.image 
     foodName.innerText = food.name
     foodCal.innerText = food.calories
+    foodCom.innerText = food.comment
+    
+
+    
+    
+   
+
+
     if (food.review == 0) {
         foodRev.innerHTML = "&#9734";
         foodRev.style.fontSize = "2rem";
         foodRev.style.color = "gray";
-    
-
     } else if (food.review ==1) {
         foodRev.innerHTML = "<p class='star'>⭐</p>";
     } else if (food.review == 2) {
@@ -62,7 +69,40 @@ function addToSubContainer(food) {
     
     const foodInfo = document.querySelector('#food-info')
     foodInfo.innerText = ""
-    foodInfo.append(foodName, foodImage, foodCal, foodRev)
+    foodInfo.append(foodName, foodImage, foodCal, foodRev, foodCom)
+
+
+    const form = document.createElement('form')
+    const review = document.createElement('input')
+    const rating = document.createElement('input')
+    const submit = document.createElement('input')
+    
+    review.name = "comment";
+    review.id = "reviewId";
+    review.type = "text";
+
+    rating.name = "rating";
+    rating.id = "ratingId"
+    rating.type = "text";
+    submit.type="submit";
+    
+    form.appendChild(review);
+    form.appendChild(submit);
+
+    console.log(review, submit);
+    
+    form.addEventListener("submit", e => {
+        e.preventDefault();
+        //Now your turn to get the data. target. New object
+        console.log(e.target.reviewId.value);
+        
+    })
+
+    
+
+
+    document.querySelector('#form-review').appendChild(form)
+    
 
     
    
