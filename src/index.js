665a1bad7cfc9de0bbaab991d4b272030ee0899e
menhaj <menhaj007@yahoo.com>
2021-04-19 11:23:37 -0400

    second commit

--- src/index.js
+++ src/index.js
@@ -16,4 +16,24 @@ function addFoods(food) {
     foodBanner.innerText = food.name
     
     document.querySelector('#banner').appendChild(foodBanner)
+
+}
+displayFood();
+function displayFood() {
+    document.querySelector('#banner').addEventListener('click', (e) => {
+        console.log(e.target.value);
+        const foodImg = document.createElement('img')
+        // foodImg.src = food.image 
+
+        const foodName = document.createElement('h4')
+        // foodName.innerText = food.name
+
+        const foodCal = document.createElement('h4')
+        // foodCal.innerText = food.calories
+
+        const foodRev = document.createElement('h4')
+        // foodRev.innerText = food.review
+
+        document.querySelector('#food-info').append(foodImg, foodName, foodCal, foodRev)
+    })
 }
